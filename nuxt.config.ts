a701a46bb76c8fe7693363bf7f1e1b3fb4944d48
helmetabd <hilmiabdurrahim@gmail.com>
2024-12-27 09:29:11 +0700

    progress velzon theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/scss/config/app.scss'],
+  // css: ['~/assets/main.scss'],
+  vite: {
+    css: {
+			preprocessorOptions: {
+				scss: {
+					silenceDeprecations: ["legacy-js-api"]
+				}
+			}
+		}
+  }
 })
